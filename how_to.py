eb309198a3919bec80b31afaf0d7d628953b5970
JY <jylee1@yonei.ac.kr>
2020-09-21 16:20:02 +0900

    2020-09-21 commit how_to.py file

--- how_to.py
+++ how_to.py
@@ -10,9 +10,11 @@ git은 프로그램, gibhub는 오픈소스 올리는 사이트
 - add configuration
 - git init: vcs -> import -> create git repository
 - git add: 에드를 해야 커밋 가능- 오른쪽 클릭-> 깃 -> 에드
-- commit: 상단의 git -> commit file
-아래 터미널 쪽에 git -> log
-
+- commit: 상단의 git -> commit file (검정색으로 변함)
+아래 터미널 쪽에 git -> log로 커밋된거 확인 가능
+- remote에 push 하기, 주소 등록해야 함
+vcs -> git remote로 주소 넣기
+vcs -> push
 
 ------
 - push를 해야함. github.com에 새 repository 주소 등록 -> 주
